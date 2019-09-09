5b7ce8eaa9744455ccd8d789aaa5385d6f8273f3
Maruf-Robin <maruf.robin131313@gmail.com>
2019-09-09 17:22:20 +0600

    First add

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name : ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + "!");
+            Console.ReadKey();
+
+        }
+    }
+}
